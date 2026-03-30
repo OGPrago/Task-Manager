fd5079ddeb26c3d96dc3b33ec1c6e890041808f3
Jeremy Garcia <jeremyg@Jeremys-MacBook-Pro.local>
2026-03-29 21:21:45 -0400

    feat: add storage layer for loading and saving task in JSON

---
+++ storage.py
@@ -0,0 +1,18 @@
+from task import Task
+import json
+
+def load():
+    try:
+        with open("task.json", 'r') as file:
+            data_list = json.load(file)
+            converted_data_list = []
+            for i in data_list:
+                converted_data_list.append(Task.from_dict(i))
+            return converted_data_list
+    except FileNotFoundError:
+        return []
+
+def save(tasks):
+    with open("task.json", 'w') as file:
+        converted_list = [task.to_dict() for task in tasks]
+        json.dump(converted_list, file)
